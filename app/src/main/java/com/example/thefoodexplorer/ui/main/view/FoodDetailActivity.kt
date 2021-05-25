902016e5f38b7f5c08d9c6f22d92915bb306582f
waff <waffiqmaaroja1998@gmail.com>
2021-05-25 11:50:55 +0700

    implement detail top ui

--- app/src/main/java/com/example/thefoodexplorer/ui/main/view/FoodDetailActivity.kt
+++ app/src/main/java/com/example/thefoodexplorer/ui/main/view/FoodDetailActivity.kt
@@ -2,15 +2,80 @@ package com.example.thefoodexplorer.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.thefoodexplorer.R
+import com.example.thefoodexplorer.data.model.FoodQuery
+import com.example.thefoodexplorer.databinding.ActivityFoodDetailBinding
+import com.example.thefoodexplorer.databinding.ActivityHomeBinding
+import com.example.thefoodexplorer.ui.base.ViewModelFactory
+import com.example.thefoodexplorer.ui.main.viewmodel.DetailViewModel
+import com.example.thefoodexplorer.ui.main.viewmodel.HomeViewModel
 
 class FoodDetailActivity : AppCompatActivity() {
     companion object{
         const val DATA = "food-data"
     }
 
+    private lateinit var binding: ActivityFoodDetailBinding
+    private lateinit var viewModel: DetailViewModel
+    private var data : FoodQuery? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_detail)
+        binding = ActivityFoodDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+            data = intent.getSerializableExtra(DATA) as FoodQuery
+        }catch (e: Exception){}
+
+        setupViewModel()
+        setupUI()
+        setupObserver()
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProvider(
+            this@FoodDetailActivity,
+            ViewModelFactory.getInstance()
+        )[DetailViewModel::class.java]
+    }
+
+    private fun setupUI() {
+        binding.back.setOnClickListener { finish() }
+        binding.btnDetail.setOnClickListener{ viewModel.setDetailUI() }
+        binding.btnLocation.setOnClickListener{ viewModel.setLocationUI() }
+        data?.let { food ->
+            Glide.with(this)
+                .load(food.image)
+                .into(binding.image)
+            binding.name.text = food.name
+            binding.city.text = food.city
+        }
+    }
+
+    private fun setupObserver() {
+        viewModel.getDetailStatus().observe(this, {
+            setButtonView(binding.btnDetail, it)
+        })
+        viewModel.getLocationStatus().observe(this, {
+            setButtonView(binding.btnLocation, it)
+        })
+    }
+
+    private fun setButtonView(btn: AppCompatButton, active: Boolean){
+        when(active){
+            true -> {
+                btn.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.normal_blur))
+                btn.setTextColor(ContextCompat.getColor(applicationContext, R.color.dark))
+            }
+            else -> {
+                btn.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.white))
+                btn.setTextColor(ContextCompat.getColor(applicationContext, R.color.medium_blur))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/thefoodexplorer/ui/main/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/thefoodexplorer/ui/main/viewmodel/DetailViewModel.kt
@@ -1,13 +1,36 @@
 package com.example.thefoodexplorer.ui.main.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.thefoodexplorer.data.repositories.FoodRepository
 
 class DetailViewModel (private val repository: FoodRepository): ViewModel(){
     private var foodID = -1
+    private var detailActive = MutableLiveData<Boolean>()
+    private var locationActive = MutableLiveData<Boolean>()
+
+    init {
+        detailActive.postValue(true)
+        locationActive.postValue(false)
+    }
+
     fun setFoodID(id: Int){
         foodID = id
     }
+
+    fun setDetailUI() {
+        detailActive.postValue(true)
+        locationActive.postValue(false)
+    }
+
+    fun setLocationUI() {
+        detailActive.postValue(false)
+        locationActive.postValue(true)
+    }
+
+    fun getDetailStatus(): LiveData<Boolean> = detailActive
+    fun getLocationStatus(): LiveData<Boolean> = locationActive
 
     fun getDetailFood() = repository.getDetailFood(foodID)
     fun getLocationFood() = repository.getLocationFood(foodID)
