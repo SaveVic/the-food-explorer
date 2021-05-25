f1935c68c0b4009e27dddce9d2ffb709ad84b01c
waff <waffiqmaaroja1998@gmail.com>
2021-05-25 09:15:41 +0700

    complete repository

--- app/src/main/java/com/example/thefoodexplorer/data/repositories/FoodRepository.kt
+++ app/src/main/java/com/example/thefoodexplorer/data/repositories/FoodRepository.kt
@@ -2,9 +2,9 @@ package com.example.thefoodexplorer.data.repositories
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.thefoodexplorer.data.api.ApiHelper
-import com.example.thefoodexplorer.data.api.FoodQueryImageCallback
-import com.example.thefoodexplorer.data.api.FoodQueryTextCallback
+import com.example.thefoodexplorer.data.api.*
+import com.example.thefoodexplorer.data.model.FoodDetail
+import com.example.thefoodexplorer.data.model.FoodLocation
 import com.example.thefoodexplorer.data.model.FoodQuery
 import com.example.thefoodexplorer.util.ApiResponse
 import kotlinx.coroutines.CoroutineScope
@@ -49,6 +49,42 @@ class FoodRepository private constructor(private val helper: ApiHelper){
             try {
                 helper.getFoodByImage(image, object : FoodQueryImageCallback{
                     override fun onGetData(data: List<FoodQuery>) {
+                        liveData.postValue(ApiResponse.success(data))
+                    }
+                })
+            }catch (e: Exception){
+                liveData.postValue(ApiResponse.error(e.toString()))
+            }
+
+        }
+        return liveData
+    }
+
+    fun getDetailFood(id: Int) : LiveData<ApiResponse<FoodDetail>>{
+        val liveData = MutableLiveData<ApiResponse<FoodDetail>>()
+        liveData.postValue(ApiResponse.loading())
+        CoroutineScope(IO).launch {
+            try {
+                helper.getDetailFood(id, object : FoodDetailCallback{
+                    override fun onGetData(data: FoodDetail) {
+                        liveData.postValue(ApiResponse.success(data))
+                    }
+                })
+            }catch (e: Exception){
+                liveData.postValue(ApiResponse.error(e.toString()))
+            }
+
+        }
+        return liveData
+    }
+
+    fun getLocationFood(id: Int) : LiveData<ApiResponse<FoodLocation>>{
+        val liveData = MutableLiveData<ApiResponse<FoodLocation>>()
+        liveData.postValue(ApiResponse.loading())
+        CoroutineScope(IO).launch {
+            try {
+                helper.getLocationFood(id, object : FoodLocationCallback{
+                    override fun onGetData(data: FoodLocation) {
                         liveData.postValue(ApiResponse.success(data))
                     }
                 })
